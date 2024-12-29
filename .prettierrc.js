fb1bf432bd2de7c49e5ebe9a74186f32d887af18
Jjfjdjdjc <ry76rt3ye@yandex.com>
2024-12-29 16:33:28 +0700

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  // Use single quotes for strings
+  singleQuote: true,
+
+  // Add a trailing comma where valid (ES5+)
+  trailingComma: 'es5',
+
+  // Use 2 spaces for indentation
+  tabWidth: 2,
+
+  // Use spaces instead of tabs
+  useTabs: false,
+
+  // Add a semicolon at the end of statements
+  semi: true,
+
+  // Print spaces between brackets in object literals
+  bracketSpacing: true,
+
+  // Put the `>` of a multi-line JSX element at the end of the last line
+  jsxBracketSameLine: false,
+
+  // Wrap prose in Markdown files when it exceeds the print width
+  proseWrap: 'always',
+
+  // Specify the line length that the printer will wrap on
+  printWidth: 80,
+
+  // Ensure parentheses are used around arrow function parameters when needed
+  arrowParens: 'always',
+
+  // Format embedded code in Markdown files
+  embeddedLanguageFormatting: 'auto',
+
+  // Use the default end of line character (lf for Linux/Mac, crlf for Windows)
+  endOfLine: 'auto',
+
+  // Format quoted properties consistently
+  quoteProps: 'as-needed',
+
+  // Format code in HTML, Vue, Angular, etc. files
+  htmlWhitespaceSensitivity: 'css',
+
+  // Format code in Vue files
+  vueIndentScriptAndStyle: false,
+};
